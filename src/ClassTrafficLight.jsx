5507f4468bb4bbc3f21bef6f2e457b8cebea0523
Jonathan Ramirez <77942993+jlramirez1234@users.noreply.github.com>
2023-09-07 09:48:23 -0500

    edit to make lights turn on individually using components

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,18 +1,42 @@
-import { Component } from "react";
+import React from "react";
 
-export class ClassTrafficLight extends Component {
+const colorPatterns = [
+  ["red", "black", "black"],
+  ["black", "black", "green"],
+  ["black", "yellow", "black"]
+];
+
+export class ClassTrafficLight extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lightIndex: 0
+    };
+  }
+
+  handleNextState = () => {
+    this.setState((prevState) => ({
+      lightIndex: (prevState.lightIndex + 1) % colorPatterns.length
+    }));
+  };
+
   render() {
+    const currentLights = colorPatterns[this.state.lightIndex];
+
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${currentLights[0]}`}></div>
+          <div className={`circle ${currentLights[1]}`}></div>
+          <div className={`circle ${currentLights[2]}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.handleNextState}>
+          Next State
+        </button>
       </div>
     );
   }
 }
+
+
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,17 +1,46 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+  const colorPatterns = [
+    // First Click
+    ["red", "black", "black"],
+    // Click Two
+    ["black", "black", "green"],
+    // Click Three
+    ["black", "yellow", "black"],
+  ];
+
+  const [patternIndex, setPatternIndex] = useState(0);
+
+ 
+  const handleNextState = () => {
+    const nextIndex = (patternIndex + 1) % colorPatterns.length;
+    setPatternIndex(nextIndex);
+  };
+
+
+  const currentPattern = colorPatterns[patternIndex];
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div
+          className="circle"
+          style={{ backgroundColor: currentPattern[0] }}
+        ></div>
+        <div
+          className="circle"
+          style={{ backgroundColor: currentPattern[1] }}
+        ></div>
+        <div
+          className="circle"
+          style={{ backgroundColor: currentPattern[2] }}
+        ></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={handleNextState}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
